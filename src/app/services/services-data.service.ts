c6a5aeb1e52ec42c3844661b9a58270189e23420
rewell namu <riwenamu@gmail.com>
2025-12-03 12:44:09 +0300

    added alias @env

--- src/app/services/services-data.service.ts
+++ src/app/services/services-data.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { tap, catchError } from 'rxjs/operators';
-import { environment } from '../environments/environment';
+import { environment } from '@environments/environment';
 
 export interface CountyService {
   id: string;
